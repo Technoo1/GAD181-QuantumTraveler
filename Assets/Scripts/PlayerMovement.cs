108dcab65ae29b8f75f0f6b27b1cd04a068ec54e
Alex <1028844@student.sae.edu.au>
2022-10-19 11:17:43 +0800

    Updated Alex's Minigame

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   [SerializeField] private float speed; 
+   private Rigidbody body; 
+
+   private void Awake() 
+   { 
+       body = GetComponent<Rigidbody>(); 
+   }
+
+   private void Update()
+   {
+       body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+       if (Input.GetKey(KeyCode.Space))
+       
+       //"speed" means the jump velocity is the same as movement velocity. Can be adjusted by replacing with number. 
+       body.velocity = new Vector2(body.velocity.x, speed);
+   }
+}
