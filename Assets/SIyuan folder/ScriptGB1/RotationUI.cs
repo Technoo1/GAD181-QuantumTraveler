1dc93986c0e057223a6fda399b024a0aacaf452e
Siyuan Shen Desktop 2 campus <1018131@student.sae.edu.au>
2022-10-26 12:08:36 +0800

    Slightly update V1.02
    Greybox soldier boy 26/10/2022

--- Assets/SIyuan folder/ScriptGB1/RotationUI.cs
+++ Assets/SIyuan folder/ScriptGB1/RotationUI.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class RotationUI : MonoBehaviour
 {
-    public float degreesPerSecond = 10.0f;
-    public float DSpeed2 = 25.0f;
-    public float DSpeed3 = 65.0f;
     public InnerTimerDraft1 checkT1; //Call from the Timer script.
+    public float degreesPerSecond = 10.0f;
+    public float DSpeed2 = 35.0f;
+    public float DSpeed3 = 70.0f;
+    
     public void Update()
     {
-        transform.Rotate(0, 0, degreesPerSecond * Time.deltaTime);
+        transform.Rotate(0, 0, degreesPerSecond * Time.deltaTime); //z axis rotation.
 
         if (checkT1.difficultyL2 == true)
         {
