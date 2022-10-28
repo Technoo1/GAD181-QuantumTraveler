d54bb8e41670f39d2440eb8a78c89c82279d2db2
Alex <1028844@student.sae.edu.au>
2022-10-28 15:29:33 +0800

    Healthbar UI
    (still working on animations)

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class EnemyChase : MonoBehaviour
 {
-    public GameObject player; 
-    public float speed; 
-    private float distance; 
+    public Transform player;
+    public float speed;
+
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position; 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime); 
+
+        Vector3 pos = new Vector3(player.position.x, transform.position.y, transform.position.z);
+
+        transform.position = Vector3.MoveTowards(this.transform.position, pos, speed * Time.deltaTime);
+
     }
 }
