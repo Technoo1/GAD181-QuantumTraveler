f7d9a4debccc9c889f4c345fe06e168316310ab2
Alex <1028844@student.sae.edu.au>
2022-11-02 13:18:28 +0800

    Bug testing update

---
+++ BUG TESTING/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int damage;
+    public Health playerHealth;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(4);
+        }
+    }
+}
