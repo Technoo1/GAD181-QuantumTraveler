4af0680a5ff7c133be41f388ed938874c344fb2e
Alex <1028844@student.sae.edu.au>
2022-11-06 15:13:57 +0800

    Fame Trap Animations Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed; 
     private Rigidbody body; 
-    private Animator anim; 
-
+    private Animator anim;
+    
     private void Awake() 
     { 
         body = GetComponent<Rigidbody>(); 
@@ -17,28 +17,28 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        body.velocity = new Vector3(horizontalInput * speed, body.velocity.y, body.velocity.z);
 
-        /*flip character when moving left and right 
+        //flip character when moving left and right 
         if (horizontalInput > 0.01f)
-            transform.localScale = Vector2.one; 
-        else if (horizontalInput < 0.01f) 
-            transform.localScale = new Vector2(-1, 1);
-            */
+            transform.localScale = Vector3.one; 
+        else if (horizontalInput < -0.01f) 
+            transform.localScale = new Vector3(-1, 1, 1);
+            
         
         if (Input.GetKey(KeyCode.Space))
         Jump();
 
         //set animator parameters
-        anim.SetBool("Walk", horizontalInput != 0);
+        anim.SetBool("walk", horizontalInput != 0); 
     }
 
-     //"speed" means the jump velocity is the same as movement velocity. Can be adjusted by replacing with number. 
-        // jump is more vertical when jumpspeed > movementspeed
-        
     private void Jump()
     {
-    body.velocity = new Vector2(body.velocity.x, 15);
+        //"speed" means the jump velocity is the same as movement velocity. Can be adjusted by replacing with number. 
+        // jump is more vertical when jumpspeed > movementspeed
+        body.velocity = new Vector2(body.velocity.x, 15);
+         
     }
 
 }
