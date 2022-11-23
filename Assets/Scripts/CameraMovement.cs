f177768dac64289366bd3244bfe547c5b70bf819
Daniel Mcintosh <1025756@student.sae.edu.au>
2022-11-23 14:20:27 +0800

    TopGun update

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,12 +6,24 @@ namespace TG
 
 public class CameraMovement : MonoBehaviour
 {
-   public float cameraSpeed;
+   public InnerTimerDraft1 TimerG2;
+   public float cameraSpeed = 10f;
+   public float FlySpeed2 = 25f;
+   public float FlySpeed3 = 50f;
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+
+        if (TimerG2.difficultyL2 == true)
+        {
+            cameraSpeed = FlySpeed2;
+        }
+        else if (TimerG2.difficultyL3 == true)
+        {
+            cameraSpeed = FlySpeed3;
+        }
     }
 }
 
