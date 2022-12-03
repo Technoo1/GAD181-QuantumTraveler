b87eeacb9e478e7715cea960c640b5e658cad6be
Daniel Mcintosh <1025756@student.sae.edu.au>
2022-12-03 12:55:45 +0800

    Top Gun Update

--- Assets/Scripts/UI Related/BackGroundMusicControllerTG.cs
+++ Assets/Scripts/UI Related/BackGroundMusicControllerTG.cs
@@ -3,33 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace TG
 {
-    public class BackGroundMusicController : MonoBehaviour
+    public class BackGroundMusicControllerTG : MonoBehaviour
     {
         public AudioSource BackGroundM2;
         public InnerTimerDraft1 TimeCheckSFX;
         public TotalUIcontriller checkCanvasB;
         public AudioSource phaseMenuM1;
+        public AudioSource GameOverSFX; //6 seconds length.
         public bool phaseMusic;
-        //public AudioSource EnemyShoot;
+        public bool GameOverMusic;
+        
+        public GameObject SirenSound;
         void Start()
         {
             phaseMusic = false;
+            GameOverMusic = false;
         }
+            
 
         void Update()
         {
-            if (TimeCheckSFX.difficultyL2 == true)
-            {
-                
-            }
 
-            if (checkCanvasB.CanvasCheckB == true && phaseMusic == false)
+            if (checkCanvasB.CanvasCheckB == true && GameOverMusic == false && phaseMusic == false)
             {
                 BackGroundM2.volume = 0.0f;
-                //EnemyShoot.volume = 0.0f;
+                
+                GameOverSFX.Play();
                 phaseMenuM1.Play();
+                SirenSound.SetActive(false);
+                GameOverMusic = true;
                 phaseMusic = true;
+
             }
+            
+         
         }
+        
     }
 }
