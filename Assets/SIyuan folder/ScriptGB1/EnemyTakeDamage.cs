b7c586435283b4b453d720cae9d998f633bbbe54
Siyuan Shen <1018131@student.sae.edu.au>
2022-12-03 12:55:21 +0800

    SFX iterate.
    Ver1.2

--- Assets/SIyuan folder/ScriptGB1/EnemyTakeDamage.cs
+++ Assets/SIyuan folder/ScriptGB1/EnemyTakeDamage.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class EnemyTakeDamage : MonoBehaviour
 {
     public GameObject EnemyDeathAni;
+    public AudioSource EnemyDeath;
      
     private void OnCollisionEnter(Collision collision)  //method of "using collision condition to check status"
     {
         if (collision.gameObject.tag == "PlayerBullet") //check with tag
         {
+            EnemyDeath.Play();
             Instantiate(EnemyDeathAni, transform.position, Quaternion.identity);
             //Destroy(collision.gameObject);
             Destroy(this.gameObject); //destroy this gameobject itself
